fb17b0a1bf89c284ffbb99ad81e5dc6f92aa15c0
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-12-16 22:45:52 +0530

    Test to check that two numbers have opposite sign

---
+++ testOppositeSign.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int num1,num2;
+	cout <<"ENTER THE FIRST NUMBER "<< endl;
+	cin>>num1;
+	cout <<"ENTER THE SECOND NUMBER"<< endl;
+	cin>> num2;
+	if((num1*num2)<0)
+	cout <<"TWO NUMBERS HAVE OPPOSITE SIGN "<< endl;
+	else
+	cout <<"TWO NUMBRS HAVE SAME SIGN "<<endl;
+	
+return 0;
+}
