6c4e0b96458579b4e65e3d2480a05c6b64d94066
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-12-16 17:04:47 +0530

    Binary to Decimal conversion

--- binaryToDecimal.cpp
+++ binaryToDecimal.cpp
@@ -2,31 +2,51 @@
 #include<cmath>
 using namespace std;
 
-int binaryToDecimal(int el){
-	long result;
+class Conversion{
+  	public:
+	int array[10];
+ 	int size;
+ 	Conversion();
+ 	void getNumber();
+ 	void display();
+ 	void binaryToDecimal();
+};
 
-		int num=el/10;
-		int i=0;
-		while(el!=0){
-			result=result+num*pow(2,i);
-			i++;
-		}
-
-	return result;
+Conversion :: Conversion(){
+  for (int i=0; i< size;i++)
+	 array[i]=0;
 }
-int main()
-{
- int size;
- int el;
- cout << "ENTER THE NO OF BITS OF BINARY NUMBER" << endl;
- cin >>size;
 
- long result=binaryToDecimal(el);
- cout <<" THE VALUE IN DECIMAL IS :"<< endl;
- cout << result;
+void Conversion ::getNumber(){
+	cout << "ENTER THE NUMBER OF BITS IN BINARY"<< endl;
+	cin >> size;
+	cout<< "ENTER THE BITS  0/1 "<< endl;
+	for(int i=0;i<size;i++){
+	   cout << "ENTER "<< i<<" bit =" << endl;
+	   cin >> array[i];
+	}
+}
 
+void Conversion ::display(){
+	cout <<"THE BINARY NUMBER IS "<< endl;
+	for (int i=0;i<size;i++)
+	     cout << array[i] << " ";
+	cout<<endl;
+}
 
+void Conversion :: binaryToDecimal(){
+		int result=0,j;
+	  	for(int i=size-1,j=0;i>=0;--i,++j){
+			result=result+array[i]*pow(2,j);
+		  }
+                cout << "THE DECIMAL VALUE IS " <<  result;
+}
 
-
-return 0;
+int main(){
+ 
+ Conversion object;
+ object.getNumber();
+ object.display();
+ object.binaryToDecimal();
+ return 0;
 }
