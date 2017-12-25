6a953c0362f60c8e40811b7fa743402a9ac54d8a
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-12-25 20:35:30 +0530

    Inorder Traverse

---
+++ Trees/BST/inorderTraverse.cpp
@@ -0,0 +1,11 @@
+3include<iostream>
+#include"main_btress"
+using namespace std;
+
+void BST::inorder(Node *node){
+	if(node ==NULL)
+	    return ;
+	inorder(node->left);
+	cout << node->info<<"->";
+	inorder(node->right);
+}
