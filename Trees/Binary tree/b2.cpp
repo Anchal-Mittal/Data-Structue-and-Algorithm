cb9804d8640ffb6d8b6b5e93f681b597e589a756
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-06-19 17:34:14 +0530

    diametre

--- Trees/Binary tree/b2.cpp
+++ Trees/Binary tree/b2.cpp
@@ -2,6 +2,7 @@
 #include<queue>
 #include<map>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -25,34 +26,36 @@ class Tree{
     }  
   int height(Node *root);
   int diameter(Node *root);
-	void insert(int el);
-	void inorder(Node *root);
-	void preorder(Node * root);
-	void postorder(Node *root);
-	void vertical_order();
+  void insert(int el);
+  void inorder(Node *root);
+  void preorder(Node * root);
+  void postorder(Node *root);
+  void vertical_order();	
   void getvertical(Node *root,int hd , map <int,vector<int> > &m);
-	
 };
 
 int Tree ::diameter(Node *root){
 	if(root==NULL)
 		return 0;
-  int lheight=height(root->left);
+        int lheight=height(root->left);
 	int rheight=height(root->right);
-	return (lheight+rheight+1);
+	int ldiameter=diameter(root->left);
+	int rdiameter=diameter(root->right);
+	return max((lheight+rheight+1),max(ldiameter,rdiameter));
  }
+
 
 int Tree ::height(Node *root){
 	if(root==NULL)
 	   return 0;
 	else{
         int lheight=height(root->left);
-	    	int rheight=height(root->right);
-   if(lheight>rheight)
-      	return lheight+1;
-    else
-        return rheight+1;
-    }  
+	int rheight=height(root->right);
+        if(lheight>rheight)
+      		return lheight+1;
+        else
+        	return rheight+1;
+       }  
 }
 
 void Tree :: getvertical(Node *root,int hd,map <int,vector<int> > &m){
@@ -142,30 +145,29 @@ int main(){
     Tree tree;
     int el;
     char  ch;
-    
-	do{
+    do{
 	cout << "ENTER THE ELEMENT DO U WANT TO INSERT " << endl;
 	cin >> el;
 	tree.insert(el);
-  cout <<" IF U WANT TO INSERT MORE ELEMENT PRESS Y" << endl;
-  cin >>ch;
+        cout <<" IF U WANT TO INSERT MORE ELEMENT PRESS Y" << endl;
+        cin >>ch;
 	}while(ch=='y'|| ch=='Y');
 	cout << "VERTICAL ORDER";
-  tree.vertical_order();
-  cout << "INORDER  TRAVERSAL " ;
-	tree.inorder(tree.root);
-  cout << endl;
-  cout << "POSTORDER TRAVERSAL ";
-	tree .postorder(tree.root);
-  cout << endl;
-	cout << "PREORDER TRAVERSAL";
-	tree .preorder(tree.root);
-  cout << endl;
-  cout << "HEIGHT OF THE TREE ";
-  int height= tree.height(tree.root);
-  cout <<height;
-  cout << "DIAMETER OF THE TREE "<< endl;
-  int diameter=tree.diameter(tree.root);
-  cout << diameter;
-	return 0;
+    tree.vertical_order();
+    cout << "INORDER  TRAVERSAL " ;
+    tree.inorder(tree.root);
+    cout << endl;
+    cout << "POSTORDER TRAVERSAL ";
+    tree .postorder(tree.root);
+    cout << endl;
+    cout << "PREORDER TRAVERSAL"; 
+    tree .preorder(tree.root);
+    cout << endl;
+    cout << "HEIGHT OF THE TREE ";
+    int height= tree.height(tree.root);
+    cout <<height;
+    cout << "DIAMETER OF THE TREE "<< endl;
+    int diameter=tree.diameter(tree.root);
+    cout << diameter;
+    return 0;
 }
